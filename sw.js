dd517164135b9b0651ded7c7aaa21e65e6682536
euvinkeel <emkeel@ucsd.edu>
2021-11-04 00:05:57 -0700

    Fix syntax error

--- sw.js
+++ sw.js
@@ -5,11 +5,11 @@ const CACHE_NAME = 'lab-7-starter';
 
 const urlsToCache = [
   // '/',
-  '/assets/scripts/main.js',
+  'assets/scripts/main.js',
   // '/assets/scripts/Router.js',
   // '/assets/components/RecipeCard.js',
   // '/assets/components/RecipeExpand.js',
-  '/assets/styles/main.css',
+  'assets/styles/main.css',
 ]
 
 // Once the service worker has been installed, feed it some initial URLs to cache
@@ -27,8 +27,7 @@ self.addEventListener('install', function (event) {
     }).catch((err) => {
       console.log("SOMEONE DECIDED TO NOT DO THEIR JOB TODAY");
       console.log(err);
-    }
-  )  
+    }));
 });
 
 /**
