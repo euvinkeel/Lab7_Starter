9ce0325a27e37750e9c8aa9822d13a401ef9bedb
euvinkeel <emkeel@ucsd.edu>
2021-11-03 22:59:51 -0700

    Rename Router.js to router.js to see if Github Pages will like that instead

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'lab-7-starter';
 const urlsToCache = [
   '/',
   '/assets/scripts/main.js',
-  '/assets/scripts/Router.js',
+  '/assets/scripts/router.js',
   '/assets/components/RecipeCard.js',
   '/assets/components/RecipeExpand.js',
   '/assets/styles/main.css',
