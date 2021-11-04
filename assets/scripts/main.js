270a9bc1daa6fdf6212c92e95c5913e1d56170d0
euvinkeel <emkeel@ucsd.edu>
2021-11-03 23:39:11 -0700

    Step 2 of fixing service workers

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -60,7 +60,7 @@ function initializeServiceWorker() {
    */
    if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').then((registration) => {
+      navigator.serviceWorker.register('sw.js').then((registration) => {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, (err) => {
