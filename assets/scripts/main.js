30bd45793d64df5a066436c1d4e65949d40a9e6a
euvinkeel <emkeel@ucsd.edu>
2021-11-03 23:35:04 -0700

    Step 1 of fixing service workers

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -58,6 +58,18 @@ function initializeServiceWorker() {
    *  TODO - Part 2 Step 1
    *  Initialize the service worker set up in sw.js
    */
+   if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js').then((registration) => {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, (err) => {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+  /*
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/sw.js').then((registration) => {
@@ -66,7 +78,7 @@ function initializeServiceWorker() {
         console.log('FAILED TO REGISTER SERVICE WORKER', err);
       });
     });
-  }
+  }*/
 }
 
 /**
